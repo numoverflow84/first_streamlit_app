2ed62ca350d3ab4816ab62d4de504c3086307d7d
NumOverflow84 <111258877+numoverflow84@users.noreply.github.com>
2022-08-14 21:32:54 -0500

    Update streamlit_app.py
    Added special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,4 @@ streamlit.text('🥗 Kale, Spinach & Tocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
   
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
