2b5530aab8365ee6881332d9d82930394fd68ff1
NumOverflow84 <111258877+numoverflow84@users.noreply.github.com>
2022-08-14 21:32:17 -0500

    Update streamlit_app.py
    Added smileys

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Tocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Tocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado Toast')
+  
